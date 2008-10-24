1b27e4f947bd7c0927cc9cc4a044d083e113ef7d
Ali Afshar <aafshar@gmail.com>
2008-10-24 16:19:07 +0100

    Added required metadata to upload to Cheeseshop

--- setup.py
+++ setup.py
@@ -6,4 +6,10 @@ setup(
     version = '0.1',
     name = 'anyvc',
     packages = ['anyvc'],
+
+    description='Library to access any version control system.',
+    url='http://www.bitbucket.org/RonnyPfannschmidt/anyvc/',
+    author='Ronny Pfannschmidt',
+    author_email='Ronny.Pfannschmidt@gmx.de',
+
 )
