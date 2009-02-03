bd9475510f120a6b6eaf665d1fe71e1ed66d1824
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-02-03 09:33:38 +0100

    fixed bzr commit

--- anyvc/workdir/bzr.py
+++ anyvc/workdir/bzr.py
@@ -125,6 +125,9 @@ class Bazaar(VCSWorkDir_WithParser):
     def commit(self, paths=None, message=None, user=None):
         try:
             paths = self._abspaths(paths)
+            #XXX: this is weird
+            if paths is not None:
+                paths = map(self.wt.relpath, paths)
             self.wt.commit(message,author=user,specific_files=paths)
         except:
             #XXX: better handling
