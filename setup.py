7bdec5c646ae6967fdbe0b36f67279ddd54acbd8
Ali Afshar <aafshar@gmail.com>
2009-03-03 20:33:06 +0000

    Added readme file and link, and read it into setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 
+import os
+
 from distutils.core import setup
 
+def read_readme():
+    f = open(os.path.join('docs', 'source', 'readme.rst'))
+    readme = f.read()
+    f.close()
+    return readme
 
 setup(
     version = '0.2',
@@ -16,5 +23,6 @@ setup(
     url='http://www.bitbucket.org/RonnyPfannschmidt/anyvc/',
     author='Ronny Pfannschmidt',
     author_email='Ronny.Pfannschmidt@gmx.de',
+    long_description=read_readme(),
 
 )
