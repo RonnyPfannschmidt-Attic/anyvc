b57e05c86a60a52835fdf7a6a834021d57653aec
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-12 12:44:10 +0200

    upgrade to latest py.test api

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,11 @@ from .helpers import all_known, VcsMan
 pytest_plugins = "doctest"
 
 class ConftestPlugin:
-    def pytest_genfuncruns(self, runspec):
-        if 'mgr' not in runspec.funcargnames:
+    def pytest_genfunc(self, funcspec):
+        if 'mgr' not in funcspec.funcargnames:
             return
         for vc in all_known:
-            vcdir = runspec.config.ensuretemp(vc.__name__)
-            testdir = vcdir.mkdir(runspec.function.__name__)
-            runspec.addfuncarg('mgr', VcsMan(vc, testdir))
+            vcdir = funcspec.config.ensuretemp(vc.__name__)
+            testdir = vcdir.mkdir(funcspec.function.__name__)
+            funcspec.addcall(mgr=VcsMan(vc, testdir))
 
