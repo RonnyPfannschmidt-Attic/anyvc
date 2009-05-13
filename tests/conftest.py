81267a4ddfbb49da465909743defc3817e19ccbb
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-13 11:30:38 +0200

    update to latest py.test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,27 @@ from .helpers import all_known, VcsMan
 pytest_plugins = "doctest"
 
 class ConftestPlugin:
-    def pytest_genfunc(self, funcspec):
-        if 'mgr' not in funcspec.funcargnames:
+
+    def pytest_addoption(self, parser):
+        parser.addoption( "-V", "--vcs", action="store", default=None,
+            help="specify the vcs's to use")
+
+
+    def pytest_generate_tests(self, metafunc):
+        names = metafunc.config.option.vcs
+        if names:
+            names = names.split(",")
+
+        if 'mgr' not in metafunc.funcargnames:
             return
         for vc in all_known:
-            vcdir = funcspec.config.ensuretemp(vc.__name__)
-            testdir = vcdir.mkdir(funcspec.function.__name__)
-            funcspec.addcall(mgr=VcsMan(vc, testdir))
+            if names and vc.__name not in names:
+                continue
+            metafunc.addcall(id=vc, param=vc)
+
+    def pytest_funcarg__mgr(self, request):
+        vc = request.param
+        vcdir = request.config.ensuretemp(vc.__name__)
+        testdir = vcdir.mkdir(request.function.__name__)
+        return VcsMan(vc, testdir)
 
