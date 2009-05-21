05d6e3402ed7004f8fe767cb7bb5d6f0be4c9001
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-21 16:36:45 +0200

    switch to latest py.test api

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,30 +2,29 @@ from .helpers import all_known, VcsMan
 
 pytest_plugins = "doctest"
 
-class ConftestPlugin:
 
-    def pytest_addoption(self, parser):
-        parser.addoption( "-V", "--vcs", action="store", default=None,
-            help="specify the vcs's to use")
-
-
-    def pytest_generate_tests(self, metafunc):
-        names = metafunc.config.option.vcs
-        if names:
-            names = names.split(",")
-            assert any(vc.__name__ == name for name in names for vc in all_known), "%r not known"%(names,)
+def pytest_addoption(parser):
+    parser.addoption( "-V", "--vcs", action="store", default=None,
+        help="specify the vcs's to use")
 
 
-        if 'mgr' not in metafunc.funcargnames:
-            return
-        for vc in all_known:
-            if names and vc.__name__ not in names:
-                continue
-            metafunc.addcall(id=vc.__name__, param=vc)
+def pytest_generate_tests(metafunc):
+    names = metafunc.config.option.vcs
+    if names:
+        names = names.split(",")
+        assert any(vc.__name__ == name for name in names for vc in all_known), "%r not known"%(names,)
 
-    def pytest_funcarg__mgr(self, request):
-        vc = request.param
-        vcdir = request.config.ensuretemp(vc.__name__)
-        testdir = vcdir.mkdir(request.function.__name__)
-        return VcsMan(vc, testdir)
+
+    if 'mgr' not in metafunc.funcargnames:
+        return
+    for vc in all_known:
+        if names and vc.__name__ not in names:
+            continue
+        metafunc.addcall(id=vc.__name__, param=vc)
+
+def pytest_funcarg__mgr(request):
+    vc = request.param
+    vcdir = request.config.ensuretemp(vc.__name__)
+    testdir = vcdir.mkdir(request.function.__name__)
+    return VcsMan(vc, testdir)
 
