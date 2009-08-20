3cbea7f31e96ebf8935e9b9a56b1a60db368c483
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-08-20 21:22:08 +0200

    resuffle rename tests and add directory rename test

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -26,20 +26,3 @@ def test_repo_default_head(mgr):
         #XXX: how to propperly normalize them
         assert head.message.strip()==message.strip()
 
-def test_rename_simple(mgr):
-    repo = mgr.make_repo('repo')
-
-    with repo.transaction(message='create', author='test') as root:
-        with root.join('test.txt').open('w') as f:
-            f.write('test')
-
-    with repo.get_default_head() as root:
-        assert root.join('test.txt').exists()
-
-    with repo.transaction(message='rename', author='test') as root:
-        #XXX: check if relative names are ok for rename in the fs api
-        root.join('test.txt').rename('test_renamed.txt')
-
-    with repo.get_default_head() as root:
-        assert not root.join('test.txt').exists()
-        assert root.join('test_renamed.txt').exists()
