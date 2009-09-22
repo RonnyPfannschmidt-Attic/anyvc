20f6d83fbc67d0c3c65681eea9d352f635bc9693
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-09-23 01:40:49 +0200

    clean up test config generation, disable remote testing for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,28 +5,21 @@ from anyvc import metadata
 pytest_plugins = "doctest"
 
 
-def pytest_addoption(parser):
-    parser.addoption( "-V", "--vcs", action="store", default=None,
-        help="specify the vcs's to use")
+test_on = {
+    '%s': None,
+#XXX'remote/%s': 'python2',
+#XXX'python3/%s': 'python3',
+#XXX'jython/%s': 'jython-2.5', #XXX: slow
+#XXX:'pypy/%s': 'pypy',
+}
 
 
 def pytest_generate_tests(metafunc):
-    names = metafunc.config.option.vcs
-    if names:
-        names = names.split(",")
-        assert any(vc.__name__ == name for name in names for vc in all_known), "%r not known"%(names,)
-
-
     if 'mgr' not in metafunc.funcargnames:
         return
     for name in metadata.backends:
-        if names and name not in names:
-            continue
-                                        #remote 
-        metafunc.addcall(id=name, param=(None, name))
-        metafunc.addcall(id='remote/'+name, param=('python2', name))
-        #XXX: later, for checking that python3 will always use a remote to python2
-        # metafunc.addcall(id='python3/'+name, param=('python3', name))
+        for id, python in test_on.items():
+            metafunc.addcall(id=id%name, param=(python, name))
 
 
 def pytest_funcarg__mgr(request):
