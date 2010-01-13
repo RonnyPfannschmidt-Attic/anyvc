99abeb647a8786091dec6ef5bc08bf9095df23c1
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-01-13 20:17:40 +0100

    split statedpath doctest to many tests

--- tests/workdir/test_stated_path.py
+++ tests/workdir/test_stated_path.py
@@ -1,13 +1,12 @@
 # copyright 2008 by Ronny Pfannschmidt
-# license lgpl3 or later
-"""
->>> StatedPath('a').abspath
->>> StatedPath('a', base='b').abspath
-'b/a'
->>> StatedPath('a')
-<normal 'a'>
->>> StatedPath('./a')
-<normal 'a'>
-"""
 from anyvc.common.workdir import StatedPath
 
+def test_no_abspath():
+    assert StatedPath('a').abspath is None
+
+def test_base_to_abspath():
+    assert StatedPath('a', base='b').abspath == 'b/a'
+
+def test_repr():
+    assert repr(StatedPath('a')) == "<normal 'a'>"
+    assert repr(StatedPath('./a')) == "<normal 'a'>"
