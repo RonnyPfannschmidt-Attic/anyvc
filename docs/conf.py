5ffa5d3d90798a3773aa2fe53843b3de6a7df4de
iAli Afshar <aafshar@gmail.com>
2010-06-03 02:58:11 -0700

    Move over to the draco sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ exclude_trees = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
 
 
 # Options for HTML output
@@ -86,18 +86,21 @@ pygments_style = 'sphinx'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+# html_style = 'default.css'
+
+html_theme_path = ['.']
+html_theme = 'draco'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = release
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'tarsius.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -107,11 +110,17 @@ html_style = 'default.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
+
+html_theme_options = {
+    'tagline': 'all version control systems through a single API',
+    'bitbucket_project': 'RonnyPfannschmidt/anyvc',
+    'nosidebar': True,
+}
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
